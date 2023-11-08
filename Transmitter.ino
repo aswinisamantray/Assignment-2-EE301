e7b18af8a44da39a237c457fefc94d5925a33da5
Aswini Samantray <aswinisamantray.7149@gmail.com>
2023-11-08 22:27:18 +0530

    Update Transmitter.ino

--- Transmitter.ino
+++ Transmitter.ino
@@ -1,20 +1,9 @@
 
 void setup() {
   Serial.begin(9600);
-  //pinMode(6, OUTPUT);
 }
 
 void loop() {
-//  byte dataToSend = 0b100;
-//  
-//  for (int bitPosition = 0; bitPosition < 3; bitPosition++) {
-//    if ((dataToSend >> bitPosition) & 1) {
-//      digitalWrite(6, HIGH);
-//    } else {
-//      digitalWrite(6, LOW); 
-//}
-//delay(1000);
-//}
-  Serial.write('o');
+  Serial.write('o'); //Transmitting bitstream through TX pin of arduino
   delay(1000);
 }
